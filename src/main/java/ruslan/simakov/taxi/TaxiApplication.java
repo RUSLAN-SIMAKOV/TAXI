2de41502cd7fa4a32c70143b4e92e4407607490e
Ruslan Simakov <ruslan.simakov@spirent.com>
2022-02-18 16:12:36 +0200

    Initial commit

---
+++ src/main/java/ruslan/simakov/taxi/TaxiApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiApplication.class, args);
+    }
+
+}
