65031a9aec5ac6b95e1bf463061b353bef922144
Ruslan Simakov <ruslan.simakov@spirent.com>
2022-02-19 23:44:55 +0200

    Add Docker. Add UTs

---
+++ src/test/java/ruslan/simakov/taxi/service/CarCreationServiceTest.java
@@ -0,0 +1,6 @@
+package ruslan.simakov.taxi.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CarCreationServiceTest {
+  
+}
---
+++ src/test/java/ruslan/simakov/taxi/service/DrivingServiceImplTest.java
@@ -0,0 +1,19 @@
+package ruslan.simakov.taxi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DrivingServiceImplTest {
+
+    @Test
+    void bookCar() {
+    }
+
+    @Test
+    void moveCarsOnOneTimeUnit() {
+    }
+
+    @Test
+    void resetAllCars() {
+    }
+}
